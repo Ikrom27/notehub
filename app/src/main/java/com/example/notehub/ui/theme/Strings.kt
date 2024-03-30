80110b0bbab9cb3750b2015fdad1b7b0177386c8
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2024-03-30 18:15:07 +0300

    hide directories

--- app/src/main/java/com/example/notehub/ui/theme/Strings.kt
+++ app/src/main/java/com/example/notehub/ui/theme/Strings.kt
@@ -2,7 +2,7 @@ package com.example.notehub.ui.theme
 
 object Strings {
     const val FOLDER_MAIN = "NoteHub"
-    const val FOLDER_FAVORITE = "Favorite"
-    const val FOLDER_TEMPLATE = "Template"
-    const val FOLDER_TRASH = "Trash"
+    const val FOLDER_FAVORITE = ".Favorite"
+    const val FOLDER_TEMPLATE = ".Template"
+    const val FOLDER_TRASH = ".Trash"
 }
