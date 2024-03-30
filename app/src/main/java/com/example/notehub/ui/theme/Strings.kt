6697d521c175a35519abdd976147a0ffc3ad0cd2
Ikrom <74912914+Ikrom27@users.noreply.github.com>
2024-03-30 17:58:52 +0300

    folders generation added

---
+++ app/src/main/java/com/example/notehub/ui/theme/Strings.kt
@@ -0,0 +1,7 @@
+package com.example.notehub.ui.theme
+
+object Strings {
+    const val FOLDER_FAVORITE = "Favorite"
+    const val FOLDER_TEMPLATE = "Template"
+    const val FOLDER_TRASH = "Trash"
+}
